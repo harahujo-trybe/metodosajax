2efc4e40c4d5e519712f028a57660528c849f28c
Gabriel Santos <harahujo@gmail.com>
2023-12-18 16:34:21 -0300

    Escrita .js Inicial

--- AJax/script.js
+++ AJax/script.js
@@ -1,36 +1,30 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const titulo = document.createElement('h1');
-    titulo.id = 'titulo';
-    titulo.innerText = 'Feito com JavaScript';
-    document.body.appendChild(titulo);
+const titulo = document.createElement('h1');
+titulo.id = 'titulo';
+titulo.innerText = 'Buscador de Constantes em Repositorio GIThub'
+document.body.appendChild(titulo);
 
-    document.getElementById('botao').addEventListener('click', function(){
-        // Obtém o caractere do campo de formulário
-        var caractere = document.getElementById('caractere').value;
-        fetch('https://raw.githubusercontent.com/harahujo-trybe/40pontos/criando_pixel_board/script.js')
-            .then(response => response.text())
-            .then(data => {
-                // Encontra a primeira ocorrência do caractere
-                var posicao1 = data.indexOf(caractere);
-                if (posicao1 != -1) {
-                    // Encontra a próxima ocorrência do caractere
-                    var posicao2 = data.indexOf(caractere, posicao1 + 1);
-                    if (posicao2 != -1) {
-                        // Extrai os caracteres entre as duas ocorrências do caractere
-                        var textoEntre = data.substring(posicao1 + 1, posicao2);
-                        alert('Os caracteres entre as duas ocorrências de "' + caractere + '" são: "' + textoEntre + '"');
-                    } else {
-                        alert('Não há uma segunda ocorrência de "' + caractere + '".');
-                    }
+
+$(document).ready(function(){
+    $('#botao').click(function(){
+        // Obtém os caracteres dos campos de formulário
+        const caractereInicial = $('#caractereInicial').val();
+        const caractereFinal = $('#caractereFinal').val();
+        $.ajax({
+            url: "https://raw.githubusercontent.com/harahujo-trybe/40pontos/criando_pixel_board/script.js",
+            dataType: "text",
+            success: function(data) {
+                // Encontra a primeira ocorrência do caractere inicial
+                const posicao1 = data.indexOf(caractereInicial);
+                // Encontra a primeira ocorrência do caractere final após o caractere inicial
+                const posicao2 = data.indexOf(caractereFinal, posicao1 + 2);
+                if (posicao1 != -1 && posicao2 != -1) {
+                    // Extrai os caracteres entre as duas ocorrências
+                    const textoEntre = data.substring(posicao1 + 0, posicao2);
+                    alert('Os caracteres entre "' + caractereInicial + '" e "' + caractereFinal + '" são: "' + textoEntre + '"' + caractereFinal);
                 } else {
-                    alert('O caractere "' + caractere + '" não foi encontrado na resposta do Ajax');
+                    alert('Não foi possível encontrar os caracteres especificados na resposta do Ajax');
                 }
-            });
+            }
+        });
     });
-t st
 });
-
-
-
-
-
